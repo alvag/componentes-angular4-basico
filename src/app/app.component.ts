52001f6d2c2fd23700229de6ad1bc4c281744218
Max Alva <alva85@gmail.com>
2017-09-28 18:36:03 -0500

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'app';
+
+  counterProgress = 0;
+  totalCountdown = 15;
+
+  constructor() {
+
+  }
+
+  updateProgress($event){
+    this.counterProgress = (this.totalCountdown - $event) / this.totalCountdown * 100;
+  }
+
+  countdownFinished() {
+    console.log('countdown has finished');
+  }
 }
